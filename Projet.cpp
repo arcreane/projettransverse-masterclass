ae4537e5df8b73bd2fcfbaf23deafd1652d7014e
Erwan Buisson <erwan8989@outlook.fr>
2021-03-03 16:02:30 +0100

    Detection visage

--- Projet.cpp
+++ Projet.cpp
@@ -3,27 +3,32 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <opencv2/objdetect.hpp>
 
 using namespace cv;
 using namespace std;
 
 void main(int argc, char** argv)
 {
-	VideoCapture cap(0); // le 0 correspond à la webcame 0 de son ordinateur. Source de la caméra
-	Mat img, imgGris, imgBlur, imgCanny;
+	string path = "Ressources/homme.png";
+	Mat img = imread(path);
+
+	CascadeClassifier faceCascade;	//chargement de la cascade
+	faceCascade.load("Ressources/haarcascade_frontalface_default.xml");
+
+	if (faceCascade.empty()) { cout << "XML file not loaded" << endl; } //facultatif : permet de vérifier que le fichier cml est bien chargé
+
+	vector<Rect> faces;
+	faceCascade.detectMultiScale(img, faces, 1.1, 10);
+
+	for (int i = 0; i < faces.size(); i++)
+	{
+		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+	}
 
 	while (true)
 	{
-		cap.read(img);
-
-		cvtColor(img, imgGris, COLOR_BGR2GRAY);
-		GaussianBlur(img, imgBlur, Size(3, 3), 3, 0);
-		Canny(imgBlur, imgCanny, 50, 150);
-
-
-		imshow("Image grise", imgGris); //affiche l'image en noire et blanc
-		imshow("image", img); //affiche l'image en couleur sans modification 
-		imshow("image Blur", imgCanny); //affiche l'image avec les bords détectés 
+		imshow("image", img);
 		waitKey(1);
 	}
 }
